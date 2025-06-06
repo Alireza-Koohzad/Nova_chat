1a808f9d25d19819eb0a07b734aa6a9f56b7f067
Alireza-Koohzad <alirezakhd2705@gmail.com>
2025-06-06 14:14:55 +0330

    add apiconfig to constant folder

---
+++ novachat-frontend/src/constants/apiConfig.js
@@ -0,0 +1,3 @@
+// src/constants/apiConfig.js
+export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:3000';
